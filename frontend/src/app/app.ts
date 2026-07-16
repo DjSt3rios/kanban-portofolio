7cc377334783999bb5ddd642ed10c9f9dbe0f1e7
Asterios Papadimitroulas <papadimitroulas@hotmail.com>
2026-07-16 22:55:07 +0300

    - Initial project files - Added a test API call to test Angular -> NestJS proxy configuration for local development

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('frontend');
+  constructor(private client: HttpClient) {
+    client.get('api').subscribe(res => console.log(res));
+  }
+}
