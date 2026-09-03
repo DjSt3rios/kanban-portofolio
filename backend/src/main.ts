4f132177213eb8d2b34e9386cd637bbe2447ee6f
Asterios Papadimitroulas <papadimitroulas@hotmail.com>
2026-09-03 20:47:02 +0300

    - Added CORS in backend - Added .npmrc in frontend to allow scripts to run

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,17 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // I allow all origins since this is just a demo
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.use(bodyParser.json({ limit: '50mb' }));
   const config = new DocumentBuilder().setTitle('Kanban Documentation').setVersion('1.0').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
